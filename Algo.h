d41a10b5ea668dcf3865a29d0d8f4a7cc1c08e9c
AD junior <sthep90@gmail.com>
2019-07-28 22:40:52 +0600

    Update Algo.h

--- Algo.h
+++ Algo.h
@@ -1,53 +1,49 @@
-//
-// Created by adnan on ২৭/৭/১৯.
-//
+namespace Algo{
 
-#ifndef HOME_ALGO_H
-#define HOME_ALGO_H
+    namespace rithm{
+        template <class T>
+        void merge(T *array,T *array1,int len,int len1,T *final){
+            int i,j,k;
+            i=j=k=0;
+            while(i<len && j<len1){
+                if(bool(array[i]<array1[j])){
+                    final[k++]=array[i++];
+                }else{
+                    final[k++]=array1[j++];
+                }
+            }
+            while (i<len){
+                final[k++]=array[i++];
+            }
 
-template <class T>
-void merge(T *ar, T *ar1, int len, int len1, T *final) {
-    int i, j, k;
-    i = j = k = 0;
-    while (i < len && j < len1) {
-        if (bool(ar[i] <= ar1[j])) {
-            final[k++] = ar[i++];
-        } else {
-            final[k++] = ar1[j++];
+            while (j<len1){
+                final[k++]=array1[j++];
+            }
+        }
+
+        template <class T>
+        void MergeSort(T *array,int left,int n){
+            if(n>=2){
+                int mid=(left+n)/2;
+                T *lArr=new T[mid];
+                T *rArr=new T[n-mid];
+                for (int i = 0; i < mid; i++) {
+                    lArr[i]=array[i];
+                }
+                for (int j = 0; j < (n-mid); j++) {
+                    rArr[j]=array[j+mid];
+                }
+                MergeSort(lArr,0,mid);
+                MergeSort(rArr,mid,n-mid);
+                merge(lArr,rArr,mid,n-mid,array);
+            }
         }
     }
-    while (i < len) {
-        final[k++] = ar[i++];
+    template <class T>
+    void srt(T *Array,int n){
+        rithm::MergeSort(Array,0,n);
     }
-    while (j < len1) {
-        final[k++] = ar1[j++];
-    }
+
 
 
 }
-
-template <class T>
-void ms(T *Array, int low, int n) {
-    if (n >= 2) {
-        int mid = (low + n) / 2;
-        T *left = new T[mid];
-        for (int i = 0; i < mid; i++) {
-            left[i] = Array[i];
-
-        }
-
-        T *right = new T[n - mid];
-        for (int j = 0; j < (n - mid); j++) {
-            right[j] = Array[j + mid];
-
-        }
-        ms(left, 0, mid);
-        ms(right, mid, n - mid);
-        merge(left, right, mid, n - mid, Array);
-    }
-
-
-}
-
-
-#endif //HOME_ALGO_H
